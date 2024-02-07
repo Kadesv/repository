39e88cddd82bca05573c5f451df9fd814b660dee
DALLIN TANNER <dallintanner@protonmail.com>
2024-02-06 21:09:45 -0700

    added html and css for page two

---
+++ src/PageTwo.jsx
@@ -0,0 +1,22 @@
+
+
+export default function PageTwo() {
+
+
+  return (
+    <>
+    Page Two
+
+    Picture
+
+    Jessica Randall
+    London, United Kingdom
+    "Front-end developer and avid reader."
+    <Button>GitHub</Button>
+    <Button>Frontend Mentor</Button>
+    <Button>LinkedIn</Button>
+    <Button>Twitter</Button>
+    <Button>Instagram</Button>
+    </>
+  );
+}
